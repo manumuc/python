4acbca496aa88f368719f658e95b500b4c8b76f6
manumuc <33813467+manumuc@users.noreply.github.com>
2019-01-20 16:19:35 +0100

    Create autodepl-lamp-co7-freebsd10.py

---
+++ autodepl-lamp-co7-freebsd10.py
@@ -0,0 +1,13 @@
+# Purpose: 
+#   deploy and full Lamp  ( Apache /MariaDB server /Php) stack  installation from your laptop  or  local computer to your Centos 7 or FreeBSD 10.x server.  
+# Result:
+#   LAmP installed on Centos 7 or FreeBSD 
+# Ingredense 
+#   Admin PC 
+#   Destination Server (puppetnode1) 
+#   Destination Server (puppetnode2) 
+# Time to do 
+
+# Import: 
+#   Os 
+#   sys
